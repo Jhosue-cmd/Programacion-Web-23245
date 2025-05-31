f12a26cff0d72570c703a61b4d228413c16ed1a6
Alfonso Arroyo <arroyoalfonso1031@gmail.com>
2025-05-31 17:35:32 -0500

    project3

---
+++ PRY-Proyecto Parcial 1/public/js/login.js
@@ -0,0 +1,7 @@
+function cambiarPagina(url) {
+    //url=views/nombre_stio.html
+    fetch(url)
+        .then(res => res.text())
+        .then(data => document.getElementById('main').innerHTML = data)
+}
+window.onload = () => cambiarPagina("./views/login.html");
